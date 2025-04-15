a16c008096e78ade269220c64a8b84ca4f784aed
Bumjargal Bayraa <bumjargalbayraa@Bumjargals-MacBook-Air.local>
2025-04-15 16:44:31 +0800

    b

--- src/components/ExamComponents/MultipleChoice.tsx
+++ src/components/ExamComponents/MultipleChoice.tsx
@@ -25,6 +25,7 @@ export default function MultipleChoice({handleSelect}: functionType) {
         <MarkingRules />
       </div>
       <SaveQuestion text="Хадгалах" />
+      <div>ahhhaa</div>
     </div>
   );
 }
